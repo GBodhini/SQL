0f7807ce4aa79342fad833c68e18dac5d734f6bb
GBodhini <138875946+GBodhini@users.noreply.github.com>
2023-08-29 18:05:03 +0530

    Add files via upload

---
+++ aug29.sql
@@ -0,0 +1,234 @@
+---------------------------August 29------------------------------
+create database aug29
+/* using of Rollback*/
+create table sample1
+(id int identity(1,1) primary key,
+name varchar(20))
+
+truncate table sample1;
+select * from sample1
+/*Rollback will not work because we are not using begin transaction*/
+insert into sample1 values('Bodhini');
+
+/*By using of Begin transaction we can roll back the data*/
+begin transaction
+insert into sample1 values('Ajay');
+/*Rollback means deletes the data*/
+rollback;
+
+/*Commit saves the data*/
+commit;
+----------------------------------------------------------------------------
+create table sample2(
+id int identity(1,1) primary key,
+name varchar(50))
+
+select * from sample2
+
+begin transaction
+insert into sample2 values('Bodhini')
+save transaction s1
+insert into sample2 values('Ajay')
+save transaction s2
+insert into sample2 values('Nitesh');
+save transaction s3
+
+/*Rollback transaction till s2*/
+rollback transaction s2;
+ -----------------------------------------------------------------------------------------------------------
+ CREATE TABLE EMPLOYEE1
+(
+EMP_ID INT IDENTITY,
+EMP_NAME VARCHAR(30),
+EMP_SAL DECIMAL(10,2)
+)
+
+ 
+
+INSERT INTO EMPLOYEE1 VALUES('NITESH',60000.00),('RAVI',40000.00),('LAKSHMI',30000.00),('KARTHIK',50000.00);
+
+ select * from EMPLOYEE1;
+
+create table  employee_test_audit(
+	EMP_ID INT,
+	EMP_NAME VARCHAR(30),
+	EMP_SAL DECIMAL(10,2),
+	AUDIT_ACTION varchar(50),
+	AUDIT_TIMESTAMP date
+)
+
+
+ALTER TRIGGER TRGAFTERINSERT3 ON [DBO].[EMPLOYEE1]
+FOR INSERT
+AS
+BEGIN
+DECLARE @EMPID INT;
+DECLARE @EMPNAME VARCHAR(100);
+DECLARE @EMPSAL DECIMAL(10,2);
+DECLARE @AUDIT_ACTION VARCHAR(100);
+
+ 
+
+SELECT @EMPID=I.EMP_ID FROM INSERTED I;
+SELECT @EMPNAME=I.EMP_NAME FROM INSERTED I;
+SELECT @EMPSAL=I.EMP_SAL FROM INSERTED I;
+SET @AUDIT_ACTION='INSERTED RECORD --AFTER INSERT TRIGGER.';
+
+ 
+
+INSERT INTO EMPLOYEE_TEST_AUDIT (EMP_NAME,EMP_SAL,AUDIT_ACTION,AUDIT_TIMESTAMP)
+VALUES(@EMPNAME,@EMPSAL,@AUDIT_ACTION,GETDATE());
+
+ 
+ 
+PRINT 'AFTER INSERT TRIGGER FIRED.'
+END
+
+select * from EMPLOYEE_TEST_AUDIT
+DELETE FROM EMPLOYEE_TEST_AUDIT;
+insert into EMPLOYEE1 values('GOPAADNSJKF',4121);
+ 
+---------------------------------------------------------------------------------------------
+/* ------ Triggers----------*/
+/*----------------------------------------------------Inserting using Triggers--------------------------------*/
+create table one(
+name1 varchar(40)
+)
+create table one_copy(
+name2 varchar(40)
+)
+
+ 
+create trigger tgInsert1 on [dbo].[one]
+for insert
+as
+declare @name varchar(40);
+select @name=i.name1 from inserted i;
+insert into one_copy values(@name);
+
+ 
+
+insert into one values('BODHINI');
+insert into one values('anu');
+INSERT INTO ONE VALUES('CHETANA')
+
+
+select * from one;
+select * from one_copy;
+--------------------------------------------------------------------------------------------------------------------------------
+/* ------------------------------------------Inserting and deleting using triggers------------------------------------*/
+create table one1(
+name1 varchar(40)
+)
+create table one_copy1(
+name2 varchar(40)
+)
+
+
+select * from one1;
+select * from one_copy1;
+/*----------------------------------------------Inserting---------------------------------------------*/
+create trigger tgInsert4 on [dbo].[one1]
+for insert
+as
+declare @name varchar(40);
+select @name=i.name1 from inserted i;
+
+insert into one_copy1 values(@name);
+insert into one1 values('Bodhini')
+insert into one1 values('Ajay');
+
+/*-----------------------------------------Deleting----------------------------------------*/
+create trigger tgDelete3 on [dbo].[one1]
+for delete
+as
+declare @name varchar(40);
+select @name=d.name1 from deleted d;
+delete from one_copy1 where name2=@name;
+
+delete from one1 where name1='ajay';
+select * from one1;
+select * from one_copy1;
+
+/*--------------------------------------------------------Updating ------------------------------------------------------------------*/
+create trigger tgupdate3 on [dbo].[one1]
+for update
+as
+declare @name varchar(40);
+select @name=d.name1 from inserted d;
+update one_copy1 set name2=@name where name2='Bodhini';
+go
+
+update one1 set name1='gopal' where name1='Chetan';
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+-----Monthly salary table
+create table monthlysalary(
+emp_id int,
+salary_amount int
+)
+
+ 
+
+select * from monthlysalary
+
+ 
+
+--Create table for reporttable
+
+ 
+
+create table reportsalary(
+emp_id int,
+totalAmountpaid int
+)
+
+ 
+
+select * from reportsalary
+
+ 
+
+--Trigger to udate the salary
+create trigger Totalsalaryupdate on dbo.monthlysalary
+for insert
+as 
+begin
+-- Take the emp id and amount which inserted into monthlysalary table
+    Declare @emp_id int;
+    Declare @amount money;
+    Declare @total_amount money;
+
+ 
+
+    --Take the value from inserted memory table and give to variable
+     Select @emp_id = emp_id from inserted;
+     Select @amount = salary_amount from inserted;
+
+ 
+
+     -- Find if salary is present or not in report table so that we can know two things
+     --1 . IF that emp id record exist or not
+     --2. IF record exist take this existing salary
+
+ 
+
+      Select @total_amount = totalAmountpaid from reportsalary where emp_id = @emp_id;  
+
+      If(@total_amount is not null)
+        BEGIN
+            SET @total_amount = @amount + @total_amount;
+            Update reportsalary set totalAmountpaid = @total_amount where emp_id = @emp_id;
+            PRINT 'Updated';
+        END
+      else
+        BEGIN
+            Insert into reportsalary values(@emp_id, @amount);
+        PRINT 'Inserted';
+    END
+end
+
+insert into monthlysalary values(101,90000);
+insert into monthlysalary values(101,90000);
+select * from monthlysalary;
+select * from reportsalary;
---
+++ ayg28.sql
@@ -0,0 +1,217 @@
+----------August 28----------------------------------------------------------------
+create database aug28;
+--------------------------------------------- Adding 3 values------------------------------------------------------
+/*Custimized logic*/
+create function getsum(@FN int,@SN int,@Month int) returns 
+int as
+begin
+return @FN+@SN+@Month end
+
+select dbo.getsum(5,8,22)
+
+-------------------------------------------------------------------------------------------------------------------
+create table  car(car_id int primary key,car_name varchar(50),Myear varchar(50));
+
+insert into car values(101,'Audi','2020'),(102,'benz','2021'),(103,'Ferrari','2020'),(104,'Rolls royce','2023');
+select * from car;
+
+create function getcar(@Myear varchar(50)) returns table 
+as return select * from car where Myear=@Myear;
+
+select * from getcar('2020');
+select * from getcar('2023');
+select * from getcar('2024');
+-----------------------------------------------------------------------------------------------------------------------------
+/*Joining two tables using Functions*/
+create table firstbatch(f_id int,f_name varchar(50),passedyear varchar(10));
+create table Secondbatch(s_id int,s_name varchar(50),passedyear varchar(10));
+insert into firstbatch values(101,'Bodhini','2023'),(102,'Ajay','2021'),(103,'Sahith','2022');
+insert into Secondbatch values(11,'Akash','2017'),(12,'Bhuvan','2018'),(13,'Chetak','2016');
+select * from firstbatch;
+select * from Secondbatch;
+create function FS1()
+returns @new TABLE (
+        nid int,
+        nname VARCHAR(50),
+       npassedyear varchar(10)
+    )
+AS
+BEGIN
+    INSERT INTO @new
+    SELECT 
+        f_id, 
+        f_name, 
+        passedyear
+    FROM
+        firstbatch;
+
+    INSERT INTO @new
+    SELECT 
+        s_id, 
+        s_name, 
+        passedyear
+    FROM
+       Secondbatch
+    RETURN;
+END;
+select * from FS1();
+
+
+create function FS2()
+returns @new TABLE (
+        nid int,
+        nname VARCHAR(50),
+       npassedyear varchar(10)
+    ) AS
+BEGIN
+    INSERT INTO @new
+    SELECT *
+    FROM
+        firstbatch;
+		
+    INSERT INTO @new
+    SELECT 
+      *
+    FROM
+       Secondbatch
+    RETURN;
+END;
+select * from FS2();
+-------------------------------------------------------------------------------------------------------------------------------
+/*using Group by clause and add the amount of customers that they paid*/
+
+create table customers(cust_id int primary key,cust_name varchar(50));
+create table orders(ord_id int,ord_name varchar(50),cust_id int foreign key references customers(cust_id),amount money);
+insert into customers values(101,'Bodhini'),(102,'Ajay');
+select * from customers;
+insert into orders values(11,'Handwash',101,80),(12,'Sanitizer',102,100);
+insert into orders values(13,'Soap',102,35);
+select * from orders;
+
+/*group by using cust_name*/
+select customers.cust_name,sum(orders.amount) from customers inner join orders on customers.cust_id=orders.cust_id group by cust_name
+
+/*group by using cust_id*/
+select customers.cust_id,sum(orders.amount) from customers inner join orders on customers.cust_id=orders.cust_id group by customers.cust_id
+
+/*Breaking into smaller parts*/
+/*With CTE*/
+with customersTotalAmount  as(select cust_id,sum(amount) as Totalamount from orders group by cust_id)
+select customers.cust_name,CustomersTotalamount.Totalamount from customers inner join customersTotalamount on customers.cust_id=customersTotalAmount.cust_id
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/* Using of ROW_Number() which gives us unique values*/
+create table batch1(b_id int primary key ,b_name varchar(50),batchno int);
+
+insert into batch1 values(11,'Bodhini',1),(12,'Ajay',1),(13,'sahith',1);
+insert into batch1 values(14,'Ram',2),(15,'Akash',2);
+select  * from batch1;
+select b_name,batchno,ROW_NUMBER() over(partition by batchno order by batchno) from batch1;
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*using of Rank number and Dense Number */
+select b_name,batchno,ROW_NUMBER() over(partition by batchno order by batchno)as RowNumber,RANK() over(order by batchno) as RankNumber,DENSE_RANK() over(order by batchno) as DenseNumber from batch1;
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*printing 10 numbers using while loop*/
+declare @counter int;
+set @counter=1
+while(@counter<10)
+begin 
+print @counter
+if(@counter>6)
+begin 
+ break
+ end
+
+set @counter=@counter+1
+end
+-------------------------------------------------------------------------------------------------------
+/*Using of while loop*/
+create table manyentries(
+id int Identity(1,1)  primary key,
+name varchar(200));
+select * from manyentries;
+
+
+Declare @counter int;
+set @counter=1
+while(@counter<=500)
+BEGIN
+    insert into manyentries values('Bodhini')
+    set @counter=@counter+1
+END
+-----------------------------------------------------------------------------------------------------------
+------------------------------task-----------------------------------------------------------------------------------
+create table stddetails(id int primary key,name varchar(50),marks int,status varchar(50));
+insert into stddetails(id,name,marks) values(101,'Bodhini',50),(102,'Ajay',70),(103,'Chetana',90);
+insert into stddetails(id,name,marks) values(104,'Sahithi',40);
+select * from stddetails;
+SELECT ID,NAME,MARKS, IIF (MARKS>60,'PASS','FAIL') AS STATUS FROM stddetails;
+(OR)
+SELECT id,name,marks,
+CASE
+    WHEN marks <= 50.00 THEN 'FAIL'
+    WHEN marks > 50.00 THEN 'PASS'
+    ELSE 'ABSENT'
+END AS RESULT
+FROM stddetails;
+------------------------------------------------------------------------------
+
+declare @c int
+declare @marks int
+declare @total_rows int
+set @c=101
+select @total_rows = count(id) from stddetails;
+
+while(@c<=@total_rows+100)
+begin
+print @c
+select @marks = marks from stddetails where id=@c;
+print @marks
+
+if(@marks>50)
+
+update stddetails set status='pass' where id=@c;
+else
+update stddetails set status='fail' where id=@c;
+set @c=@c+1
+end
+select * from stddetails
+-------------------------------------------------------------------------------------------------------------------------
+/* Using Continue*/
+declare @counter int;
+set @counter=0;
+while(@counter<15)
+begin
+set @counter=@counter+1
+if @counter>6 and @counter<10 continue;
+print @counter
+end
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create table stdetails3
+(
+    id int identity(1,1),
+    Name varchar(50),
+    Marks1 int,
+    status varchar(10)
+);
+
+ 
+insert into stdetails3(Name,Marks1) values('Monika',90),('Gopal',50),('Nites',70),
+('Meena',60),('Prabakar',45),('Mitesh',35),('Magie',80),('Gayi',50),('Naresh',70)
+,('Chaitu',100),('Maha',45),('Bhavani',35);
+
+ SELECT id,Name,Marks1, IIF (Marks1>35,'PASS','FAIL') AS STATUS FROM stdetails3;
+
+declare @cd int
+declare @marks1 decimal
+declare @total_rows1 int 
+set @cd=1
+select @total_rows1 = count(id) from stdetails3; 
+while(@cd<=10)
+begin
+select @marks1=Marks1 from stdetails3 where id=@cd; 
+if(@marks1>35) update stdetails3 set status='pass' where id=@cd; 
+else update stdetails3 set status='fail' where id=@cd;
+set @cd=@cd+1 
+end
+select * from stdetails3
